e453814cd01356e1cb67f60b323927f79c99d787
Golubenko Liza <golubenko@extertech.com>
2016-10-20 19:48:41 +0300

    123

--- public/js/accordeon.js
+++ public/js/accordeon.js
@@ -1,7 +1,4 @@
 $(document).ready(function(){
-
-
-
     $('.accordion__title').click(function(){
         $(this).toggleClass('accordion__title_active');
         $(this).next('.accordion__content').slideToggle(400);
