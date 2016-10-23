3b6169a1f28a904b73f730628415cf4b1ff76c17
DmitrySpor <dmitrysporysh@gmail.com>
2016-10-23 15:31:45 +0300

    some fix

--- database/seeds/ReceivedPatientsTableSeeder.php
+++ database/seeds/ReceivedPatientsTableSeeder.php
@@ -19,34 +19,36 @@ class ReceivedPatientsTableSeeder extends Seeder
 
         foreach (range(1, 40) as $index) {
             DB::table('received_patients')->insert([
+                'fio' => $fios[$index-1],
+                'received_date' => '2016-10-'. ($index % 30 + 1),
+                'inspection_protocol_id' => $index,
                 'patient_id' => $index,
                 'registration_nurse_id' => 1 + ((($index - 1) % 4) * 4),
-                'fio' => $fios[$index-1],
+                'phone' => '797800007'.($index % 20 * 10),
+                'received_type' => $received_types[$index % 4],
+
                 'marital_status' => $marital_status[$index % 2],
-                'received_date' => '2016-10-'. ($index % 30 + 1),
                 'work_place' => 'Фирма № '.($index % 8 + 1),
                 'residential_address' => 'г.Севастополь, ул.Вакуленчука №'.$index,
                 'registration_address' => 'г.Севастополь, ул.Гагарина №'.$index,
-                'phone' => '797800007'.($index % 20 * 10),
-                'complaints' => 'у пацинта что-то болит. Говорит "мне прям ваще фигово"',
-                'received_type' => $received_types[$index % 4],
-                'inspection_protocol_id' => $index
+                'complaints' => 'у пацинта что-то болит. Говорит "мне прям ваще фигово"'
             ]);
         }
 
         foreach (range(41, 300) as $index) {
             DB::table('received_patients')->insert([
-                'patient_id' => $index,
-                'registration_nurse_id' => 1 + ((($index - 1) % 4) * 4),
                 'fio' => $fios[$index - 1],
-                'marital_status' => $marital_status[$index % 2],
                 'received_date' => '2016-10-10 08:'.($index % 60).':00',
+                'patient_id' => $index,
+                'phone' => '79781345'.($index * 10),
+                'received_type' => $received_types[$index % 4],
+
+                'registration_nurse_id' => 1 + ((($index - 1) % 4) * 4),
+                'marital_status' => $marital_status[$index % 2],
                 'work_place' => 'Фирма № '.($index % 25 + 1),
                 'residential_address' => 'г.Севастополь, ул.Вакуленчука №'.$index,
                 'registration_address' => 'г.Севастополь, ул.Гагарина №'.$index,
-                'phone' => '79781345'.($index * 10),
-                'complaints' => 'у пацинта что-то болит. Говорит "мне прям ваще фигово"',
-                'received_type' => $received_types[$index % 4]
+                'complaints' => 'у пацинта что-то болит. Говорит "мне прям ваще фигово"'
             ]);
         }
     }
